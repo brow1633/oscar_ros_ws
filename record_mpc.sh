be651ec62b01a370790c980af0776a31922000c4
Ethan Brown <brow1633@purdue.edu>
2024-02-10 16:22:28 -0500

    Update submodule, add ros2bag script

---
+++ record_mpc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ros2 bag record /global_path /mpc_debug_path /path /attcoveuler /atteuler /clicked_point /cmd_vel /cmd_vel_nav /cmd_vel_teleop /coverage_path /diff_cont/cmd_vel_unstamped /diff_cont/odom /gpgga /gps/filtered /gps/fix /gps/fix2 /gps/pose_as_imu /gpsfix /map /odom /odometry/global /odometry/gps /odometry/local /plan /plan_smoothed /poscovcartesian /poscovgeodetic /pose /pvtgeodetic /robot_description /rosout /speed_limit /tf /tf_static /transformed_global_plan /velcovegeodetic /zed2i/zed_node/imu/data
+
